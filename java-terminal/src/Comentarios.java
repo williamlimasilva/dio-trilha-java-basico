5bf37b07b6e308627d7d5a4d7307b33a952e0e84
williamlimasilva <williamtakuma@yahoo.com.br>
2024-04-29 15:26:35 -0300

    Explicação sobre comentarios

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,12 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha
+        /* Olá,
+         * Eu sou um comentario
+         * que posso ser mais detalhadod
+         * quando necessário
+         */
+
+    }
+}
+
