2359295d5cfe5455ee4106b6b3e818baf863b3c6
williamlimasilva <williamtakuma@yahoo.com.br>
2024-04-30 14:36:40 -0300

    Aulas sobre exceções try cath

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,29 +1,32 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
-        //criando o objeto scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {
+            //criando o objeto scanner
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome");
-        String nome = scanner.next();
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
 
-        System.out.println("Digite seu sobrenome");
-        String sobrenome = scanner.next();
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = scanner.next();
 
-        System.out.println("Digite sua idade");
-        int idade = scanner.nextInt();
+            System.out.println("Digite sua idade");
+            int idade = scanner.nextInt();
 
-        System.out.println("Digite sua altura");
-        double altura = scanner.nextDouble();
-
-
-        //imprimindo os dados obtidos pelo usuario
-        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + " cm");
+            System.out.println("Digite sua altura");
+            double altura = scanner.nextDouble();
 
 
+            //imprimindo os dados obtidos pelo usuario
+            System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + " cm");
+        }catch (InputMismatchException e){
+            System.out.println("Campos aceita apenas valores numericos");
+        }
     }
 }
