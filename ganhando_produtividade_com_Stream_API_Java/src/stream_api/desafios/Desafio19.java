97dfdaee29bb6a0cf34728cfe597e691c29cbc1e
williamlimasilva <williamtakuma@yahoo.com.br>
2024-05-10 18:08:05 -0300

    ganhando_produtividade_com_Stream_API_Java Desafio19

---
+++ ganhando_produtividade_com_Stream_API_Java/src/stream_api/desafios/Desafio19.java
@@ -0,0 +1,14 @@
+package stream_api.desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio19 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        numeros.stream()
+                .filter(n -> n % 3 == 0 || n % 5 == 0)
+                .reduce(Integer::sum)
+                .ifPresent(System.out::println);
+    }
+}
