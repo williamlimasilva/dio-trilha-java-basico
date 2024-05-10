866f8b911d7ad70f253a24c3149bded19773a642
williamlimasilva <williamtakuma@yahoo.com.br>
2024-05-10 09:00:22 -0300

    ganhando_produtividade_com_Stream_API_Java Desafio1

---
+++ ganhando_produtividade_com_Stream_API_Java/src/stream_api/desafios/Desafio1.java
@@ -0,0 +1,11 @@
+package stream_api.desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio1 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        numeros.stream().toList().forEach(n -> System.out.println(n));
+        }
+    }
